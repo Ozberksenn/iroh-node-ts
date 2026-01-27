808c2085355764d240e4ea47f0a7fb5b9ca8c36f
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2026-01-28 00:08:56 +0300

    fixed

--- src/server.ts
+++ src/server.ts
@@ -93,12 +93,29 @@ import cors from 'cors';
 import cookieParser from "cookie-parser";
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'https://playground-management.vercel.app'
+];
+
 app.use(cors({
-    origin: ['http://localhost:3000','http://localhost:5173','https://playground-management.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials:true
-  }));
+  origin: (origin, callback) => {
+    // server-to-server, postman, healthcheck
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    // ❗ ASLA Error fırlatma
+    return callback(null, false);
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 const PORT = 3001;
 app.use(express.json());
 app.use(cookieParser()); // cookie refresh token için
